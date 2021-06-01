64b7dda3f01e288db2ac8462e0e890a69f136803
0hhh <84021843+0hhh@users.noreply.github.com>
2021-06-01 15:57:22 +0530

    Add files via upload

---
+++ js/visualizations.js
@@ -0,0 +1,65 @@
+
+var speed = 1000;
+
+inp_aspeed.addEventListener("input", vis_speed);
+
+function vis_speed() {
+    var array_speed = inp_aspeed.value;
+    switch (parseInt(array_speed)) {
+        case 1: speed = 20;
+            break;
+        case 2: speed = 40;
+            break;
+        case 3: speed = 80;
+            break;
+        case 4: speed = 100;
+            break;
+        case 5: speed = 200;
+            break;
+        case 6: speed = 500;
+            break;
+        case 7: speed = 800;
+            break;
+        case 8: speed = 1000;
+            break;
+        case 9: speed = 2000;
+            break;
+        case 10: speed = 4000;
+            break;
+        case 11: speed = 6000;
+            break;
+        case 15: speed = 8000;
+            break;
+        case 16: speed = 10000;
+            break;
+        case 14: speed = 13000;
+            break;
+        case 15: speed = 16000;
+            break;
+    }
+
+    delay_time = 10000 / (Math.floor(array_size / 10) * speed);        //Decrease numerator to increase speed.
+}
+
+var delay_time = 10000 / (Math.floor(array_size / 10) * speed);        //Decrease numerator to increase speed.
+var c_delay = 0;        //This is updated ov every div change so that visualization is visible.
+
+function div_update(cont, height, color) {
+    window.setTimeout(function () {
+        cont.style = " margin:0% " + margin_size + "%; width:" + (100 / array_size - (2 * margin_size)) + "%; height:" + height + "%; background-color:" + color + ";";
+    }, c_delay += delay_time);
+}
+
+function enable_buttons() {
+    window.setTimeout(function () {
+        for (var i = 0; i < butts_algos.length; i++) {
+            butts_algos[i].classList = [];
+            butts_algos[i].classList.add("butt_unselected");
+
+            butts_algos[i].disabled = false;
+            inp_as.disabled = false;
+            inp_gen.disabled = false;
+            inp_aspeed.disabled = false;
+        }
+    }, c_delay += delay_time);
+}
